b3ccc749ca6dc18123f88ba78fd4c8b8b3ece7f1
Richard Mills <scripts.richard@gmail.com>
2017-06-11 21:23:58 -0600

    Style adjustments.

--- app/views.py
+++ app/views.py
@@ -7,8 +7,6 @@ from flask import render_template
 @app.route('/')
 def index():
     lights = hue.get_lights()
-    count = len(lights)
     return render_template('index.html',
                            title='Hue Control',
-                           lights=lights,
-                           count=count)
+                           lights=lights)
